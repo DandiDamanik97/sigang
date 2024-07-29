35a08f75b2223ea822d08533896506f8b01f0917
DandiDamanik <dandidamanik210497@gmail.com>
2024-07-29 15:14:05 +0700

    membuat halaman index,login,header dan footer php

---
+++ footer.php
@@ -0,0 +1,13 @@
+<footer class="footer">
+      <div class="container">
+        <p class="text-muted">Create By: Dandi Tornando Damanik</p>
+      </div>
+    </footer>
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="assets/js/jquery.min.js" ></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include"header.php";?>
+
+    <!-- Begin page content -->
+    <div class="container">
+      <div class="panel panel-primary">
+        <div class="panel-heading">
+          <h3 class="panel-title">Halaman Utama Sistem Informasi harGA baraNG </h3>
+               </div>
+      <div class="panel-body">
+        <img src="assets/img/indowiraputra.jpg " alt="Gambar" class="img-thumbnail" width="1000px">
+         
+    </div>
+  </div>
+
+<?php include"footer.php"; ?>
